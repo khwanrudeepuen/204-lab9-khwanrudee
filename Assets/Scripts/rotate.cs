8ef2debbd5948fc01220642d272e419c9e2ad0bb
khwanrudeepuen <khwanrudee.puen@bumail.net>
2025-03-17 09:52:10 +0700

    add scripts rotate

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] Vector3 angularV, torque;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.angularVelocity = angularV;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            rb.AddTorque(torque);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
+           
+        
+    }
+}
